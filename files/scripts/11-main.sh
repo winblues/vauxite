bef9aaeb8c112b0b1bf74b168ba4c931f6dff5b3
Adam Fidel <adam@fidel.id>
2025-02-27 19:44:37 -0600

    feat: add chezmoi

--- files/scripts/11-main.sh
+++ files/scripts/11-main.sh
@@ -8,4 +8,7 @@ echo "KillUserProcesses=yes" >>/usr/lib/systemd/logind.conf
 curl -L -o /usr/bin/xfconf-profile https://github.com/winblues/xfconf-profile/releases/latest/download/xfconf-profile-linux-amd64
 chmod +x /usr/bin/xfconf-profile
 
+curl -L -o /usr/bin/chezmoi https://github.com/twpayne/chezmoi/releases/latest/download/chezmoi-linux-amd64
+chmod +x /usr/bin/chezmoi
+
 gem install fusuma
