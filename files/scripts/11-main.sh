259397d72340dcd00f229fd67e898493167da138
Adam Fidel <adam@fidel.id>
2025-04-23 08:28:04 -0500

    feat: add flatpost

--- files/scripts/11-main.sh
+++ files/scripts/11-main.sh
@@ -14,5 +14,9 @@ chmod +x /usr/bin/chezmoi
 gem install fusuma --no-document --install-dir /usr/lib/ruby/gems/fusuma
 ln -s /usr/lib/ruby/gems/fusuma/bin/fusuma /usr/bin/fusuma
 
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release{,-extras}
+dnf5 -y install flatpost
+dnf5 -y config-manager setopt "terra*".enabled=false
+
 systemctl preset-all
 systemctl --global preset-all
